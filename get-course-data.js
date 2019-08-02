f0ae29765df96a66f5fffb2bfd6a045129600e4d
Christopher Frost <uqcfrost@uq.edu.au>
2019-08-03 09:15:47 +1000

    Muliple chunks of pageloads per file (course-list)

--- get-course-data.js
+++ get-course-data.js
@@ -2,7 +2,8 @@ const $ = require('cheerio');
 const fs = require('fs');
 const request = require('request-promise');
 
-const chunkSize = 100;
+const fileLimit = 500;
+const chunkSize = 25;
 const gaStrings = [
     ' . IN-DEPTH KNOWLEDGE OF THE FIELD OF STUDY  <br>',
     ' . A <b>comprehensive</b> and <b>well-founded knowledge</b> in the field of study. <br> ',
@@ -27,16 +28,21 @@ const gaStrings = [
     ' . A <b>knowledge</b> and <b>respect</b> of <b>ethics</b> and <b>ethical standards</b> in relation to a major area of study. <br>',
     ' . A <b>knowledge</b> of other <b>cultures</b> and <b>times</b> and an <b>appreciation</b> of <b>cultural diversity</b>. <br>',
 ];
-const recordLimit = 500;
+const recordLimit = 100001;
 const pathFragment = 'https://course-profiles.uq.edu.au/student_section_loader/print/';
 
 let data = [];
 let learningData = [];
 
 function writeFiles(recordNum) {
-    fs.writeFileSync(`./data/courses-${recordNum}.json`, JSON.stringify(data));
-    fs.writeFileSync(`./data/learning.json-${recordNum}`, JSON.stringify(learningData));
-    console.log(`length: ${data.length} in batch ${recordNum}`);
+    let ratio = (recordNum-fileLimit)/fileLimit;
+    if (ratio === Math.round(ratio)) {
+        fs.writeFileSync(`./data/courses/courses-${recordNum}.json`, JSON.stringify(data));
+        fs.writeFileSync(`./data/learning/learning-${recordNum}.json`, JSON.stringify(learningData));
+        console.log(`Saved ./data/courses/courses-${recordNum}.json`);
+        data = [];
+        learningData = [];
+    }
     if (recordNum + 1 <= recordLimit) {
         getChunk(recordNum + 1);
     }
@@ -49,7 +55,6 @@ function cleanHTML(str, options) {
         .replace(/<strong>/g, '')
         .replace(/<\/strong>/g, '')
         .replace(/ class=\"text-center\"/g, '');
-
     if (options !== undefined && options.ga) {
         for (let ga of gaStrings) {
             output = output.replace(ga, '');
@@ -61,9 +66,7 @@ function cleanHTML(str, options) {
 function getChunk(startNum) {
     let endNum = startNum + chunkSize - 1;
     let processed = 0;
-    data = [];
-    learningData = [];
-    console.log(`Getting ${startNum} - ${endNum}`);
+    console.log(`Getting chunk from ${startNum}`);
     for (let recordNum = startNum; recordNum <= endNum; recordNum++) {
         request({
             method: 'GET',
@@ -71,14 +74,12 @@ function getChunk(startNum) {
             uri: `${pathFragment}${recordNum}?print_section_1=1&print_section_7=1`, 
         })
         .then(function(response){
-        
             //success!
             const html = response.body;
-            if (html.length > 12000) {
+            if (html.length > 10000) {
                 const header = $('h1', html);
                 const headerText = header.text();
                 const breadcrumb = header.next().text().split('|');
-
                 let details = {
                     id: recordNum,
                     code: headerText.substring(0, headerText.indexOf(' - ')),
@@ -119,5 +120,5 @@ function getChunk(startNum) {
     }
 }
 
-getChunk(1);
+getChunk(78001);
 
